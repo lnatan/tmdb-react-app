be3778fb06310757c2dc3e8c7c5672cd92bf49f9
nata le <lna29ster@gmail.com>
2019-08-07 17:26:15 +0300

    Update 2019-08-07T14:26:11.750Z

--- gulp/watch.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = (gulp, plugins, config) => () => {
-  gulp.watch(config.watch.css, gulp.series('build-scss'));
-  gulp.watch(config.paths.js, gulp.series('build-js'));
-  gulp.watch(config.paths.html, gulp.series('copy'));
-};
